b8fa7ac20b99a7f19c0757d2345d44c7b18f4a64
Nattan Sousa <nattan@192.168.1.7>
2024-06-10 11:06:08 -0300

    first commit

---
+++ app/Models/Document.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Document extends Model  implements HasMedia
+{
+    use SoftDeletes;
+    use InteractsWithMedia;
+}
