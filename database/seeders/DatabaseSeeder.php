5e6aa9cf60f36ddc90c78f19ca331d7edb1106ae
Nattan Sousa <nattan_lion@msn.com>
2024-06-13 20:37:06 -0300

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,23 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //create a super admin
-        $superAdmin = User::factory()->create([
+        User::factory()->create([
             'name' => 'Nattan Sousa',
             'email' => 'nattan_lion@msn.com',
             'password' => bcrypt('password'),
         ]);
-
-        //add roles to super admin
-        $superAdmin->assignRole('super_admin');
-
-        // User::factory(10)->create();
-
-        $user = User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
-        $user->assignRole('app_user');
     }
 }
