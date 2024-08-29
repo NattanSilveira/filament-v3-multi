39a26db36e61093f53243fc8ca4b040797047436
Nattan Sousa <nattan_lion@msn.com>
2024-08-29 20:16:32 -0300

    fix

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -7,7 +7,7 @@ chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 # Executa o build do Yarn
 echo "Executando yarn run build..."
 cd /var/www || exit
-yarn run build
+#yarn run build
 
 # Coloque aqui sua lógica para esperar por serviços externos, se necessário
 
