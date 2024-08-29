a14139ce53f03cebe525ebb9929f40cda3a4a3ca
Nattan Sousa <nattan_lion@msn.com>
2024-08-29 20:14:23 -0300

    fix

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -7,4 +7,12 @@ chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 # Executa o build do Yarn
 echo "Executando yarn run build..."
 cd /var/www || exit
-#yarn run build
+yarn run build
+
+# Coloque aqui sua lógica para esperar por serviços externos, se necessário
+
+# Executa migrações do Laravel
+#php /var/www/artisan migrate --force
+
+# Inicializa o Supervisor para gerenciar os serviços
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
