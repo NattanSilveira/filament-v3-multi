bd0873769302b320fa0eef781a3e5fdcedd13627
Nattan Sousa <nattan_lion@msn.com>
2024-08-29 16:02:33 -0300

    teste job

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+//route to dispatch the job
+Route::get('/dispatch-job', function () {
+    \App\Jobs\GetDocumentsToNotifyJob::dispatch();
+    return 'Job dispatched';
+});
